2c8e713a3e6fcf5c8e9f22894b0b8ce48ad791b9
Jake Berg <jacobvictorberg@gmail.com>
2018-08-28 18:25:52 -0400

    Made arguments accept single argument to start program, cleaned up code

--- iss.py
+++ iss.py
@@ -1,18 +1,21 @@
 #!/usr/bin/env python2
 
-import re
 import sys
-import urllib
 import requests
 import argparse
 import turtle
 import time
 
+
 def find_astronauts():
+    '''Sends an api request to find out the current astronauts on the ISS'''
     r = requests.get("http://api.open-notify.org/astros.json")
     return r.json()
 
+
 def indy_time():
+    '''Sends an api request to get the next time
+     the ISS will pass over Indianapolis'''
     coords = {'lat': 39.7684, 'lon': -86.1581}
     r = requests.get("http://api.open-notify.org/iss-pass.json", params=coords)
     t = time.ctime(r.json()['response'][0]['risetime'])
@@ -20,23 +23,23 @@ def indy_time():
 
 
 def find_iss():
+    '''Creates a plot of the world map with the position of
+    the ISS and when the ISS will pass over Indianapolis'''
     r = requests.get("http://api.open-notify.org/iss-now.json",)
     iss_info = r.json()
-    # iss_timestamp = iss_info['timestamp']
     iss_position = iss_info['iss_position']
     iss_longitude = iss_position['longitude']
     iss_latitude = iss_position['latitude']
 
     screen = turtle.Screen()
-    screen.setup(720,360)
+    screen.setup(720, 360)
     screen.setworldcoordinates(-180, -90, 180, 90)
     screen.bgpic("map.gif")
-
     screen.register_shape("iss.gif")
+
     t = turtle.Turtle()
     t.shape("iss.gif")
     t.setheading(90)
-
     t.penup()
     t.goto(float(iss_longitude), float(iss_latitude))
 
@@ -48,16 +51,17 @@ def find_iss():
     location.resizemode('user')
     location.shapesize(0.2)
     location.write(indy_time())
-    screen.exitonclick() 
+
+    screen.exitonclick()
 
 
 def create_parser():
     """Creates an argument parser object"""
     parser = argparse.ArgumentParser()
-    parser.add_argument('-a','--astros',
-                        help='finds astronauts')
-    parser.add_argument('-i','--iss',
-                        help='finds iss',)
+    parser.add_argument('-a', '--astros',
+                        help='finds astronauts', action='store_true')
+    parser.add_argument('-i', '--iss',
+                        help='finds iss', action='store_true')
     return parser
 
 
@@ -70,15 +74,15 @@ def main(args):
         sys.exit(1)
 
     parsed_args = parser.parse_args(args)
-    
 
-    # Finds all astronuts and gives their craft and the number of astronauts
+
+# Finds all astronuts and gives their craft and the number of astronauts
     if parsed_args.astros:
         found_astronauts = find_astronauts()
         print """
-Number of astronauts in space:""",found_astronauts["number"]
+Number of astronauts in space:""", found_astronauts["number"]
         print """
-Astronaut names and craft: 
+Astronaut names and craft:
 """
         for astronaut in found_astronauts["people"]:
             print astronaut["name"], astronaut["craft"]
@@ -86,6 +90,7 @@ Astronaut names and craft:
     # Finds coordinates of the ISS
     if parsed_args.iss:
         find_iss()
+
 
 if __name__ == '__main__':
     main(sys.argv[1:])
